7a29e5a64619373c6d29e69261fb891c09adec5c
haad <haad@headbanggames.com>
2016-07-07 15:14:55 +0200

    Remove log messages. Add only uniques to local cache.

--- src/Store.js
+++ src/Store.js
@@ -28,7 +28,7 @@ class Store {
     return Cache.loadCache(this.options.cacheFile).then(() => {
       const cached = Cache.get(this.dbname);
       if(cached) {
-        this._lastWrite.push(cached);
+        if(this._lastWrite.indexOf(cached) > -1) this._lastWrite.push(cached);
         return Log.fromIpfsHash(this._ipfs, cached)
           .then((log) => this._oplog.join(log))
           .then((merged) => this._index.updateIndex(this._oplog, merged))
@@ -52,7 +52,7 @@ class Store {
       return Promise.resolve([]);
     }
 
-    const oldCount = this._oplog.items.length;
+    // const oldCount = this._oplog.items.length;
     let newItems = [];
     this.events.emit('sync', this.dbname);
     this._lastWrite.push(hash);
@@ -63,9 +63,9 @@ class Store {
       .then(() => Cache.set(this.dbname, hash))
       .then(() => this._index.updateIndex(this._oplog, newItems))
       .then(() => {
-        if(newItems.length > 0) {
-          console.log("Sync took", (new Date().getTime() - startTime) + "ms", this.id)
-        }
+        // if(newItems.length > 0) {
+        //   console.log("Sync took", (new Date().getTime() - startTime) + "ms", this.id)
+        // }
         this.events.emit('updated', this.dbname, newItems);
       })
       .then(() => newItems)

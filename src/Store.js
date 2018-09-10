27cc475660a48fb08219917ed2cf427bf938c624
shamb0t <shams.methnani@gmail.com>
2018-09-10 09:53:43 +0400

    ipfs-log wip

--- src/Store.js
+++ src/Store.js
@@ -22,7 +22,9 @@ const DefaultOptions = {
 }
 
 class Store {
-  constructor (ipfs, peerId, address, options) {
+  constructor (ipfs, identity, address, options) {
+    if(!identity) throw new Error("Identity required")
+
     // Set the options
     let opts = Object.assign({}, DefaultOptions)
     Object.assign(opts, options)
@@ -33,7 +35,8 @@ class Store {
 
     // Create IDs, names and paths
     this.id = address.toString()
-    this.uid = peerId
+    this.identity = identity
+    this.uid = identity.publicKey
     this.address = address
     this.dbname = address.path || ''
     this.events = new EventEmitter()
@@ -42,23 +45,21 @@ class Store {
     this._ipfs = ipfs
     this._cache = options.cache
 
-    this._keystore = options.keystore
-    this._key = options && options.key
-      ? options.key
-      : this._keystore.getKey(peerId) || this._keystore.createKey(peerId)
-    // FIX: duck typed interface
-    this._ipfs.keystore = this._keystore
+    // this._keystore = options.keystore
+    // this._key = options && options.key
+    //   ? options.key
+    //   : this._keystore.getKey(peerId) || this._keystore.createKey(peerId)
+    // // FIX: duck typed interface
+    // this._ipfs.keystore = this._keystore
 
     // Access mapping
     const defaultAccess = {
-      admin: [this._key.getPublic('hex')],
-      read: [], // Not used atm, anyone can read
-      write: [this._key.getPublic('hex')],
+      canAppend: (entry) => (entry.identity.publicKey === identity.publicKey)
     }
     this.access = options.accessController || defaultAccess
 
     // Create the operations log
-    this._oplog = new Log(this._ipfs, this.id, null, null, null, this._key, this.access.write)
+    this._oplog = new Log(this._ipfs, this.access, this.identity, this.id)
 
     // Create the index
     this._index = new this.options.Index(this.uid)
@@ -192,7 +193,7 @@ class Store {
     await this._cache.destroy()
     // Reset
     this._index = new this.options.Index(this.uid)
-    this._oplog = new Log(this._ipfs, this.id, null, null, null, this._key, this.access.write)
+    this._oplog = new Log(this._ipfs, this.access, this.identity, this.id)
     this._cache = this.options.cache
   }
 
@@ -208,7 +209,7 @@ class Store {
 
     await mapSeries(heads, async (head) => {
       this._recalculateReplicationMax(head.clock.time)
-      let log = await Log.fromEntryHash(this._ipfs, head.hash, this._oplog.id, amount, this._oplog.values, this._key, this.access.write, this._onLoadProgress.bind(this))
+      let log = await Log.fromEntryHash(this._ipfs, this.access, this.identity, head.hash, this._oplog.id, amount, this._oplog.values, this._onLoadProgress.bind(this))
       await this._oplog.join(log, amount)
     })
 
@@ -239,10 +240,10 @@ class Store {
         return Promise.resolve(null)
       }
 
-      if (!this.access.write.includes(head.key) && !this.access.write.includes('*')) {
-        console.warn("Warning: Given input entry is not allowed in this log and was discarded (no write access).")
-        return Promise.resolve(null)
-      }
+      // if (!this.access.write.includes(head.key) && !this.access.write.includes('*')) {
+      //   console.warn("Warning: Given input entry is not allowed in this log and was discarded (no write access).")
+      //   return Promise.resolve(null)
+      // }
 
       // TODO: verify the entry's signature here
 
@@ -397,7 +398,7 @@ class Store {
       const snapshotData = await loadSnapshotData()
       this._recalculateReplicationMax(snapshotData.values.reduce(maxClock, 0))
       if (snapshotData) {
-        const log = await Log.fromJSON(this._ipfs, snapshotData, -1, this._key, this.access.write, 1000, onProgress)
+        const log = await Log.fromJSON(this._ipfs, this.access, this.identity, snapshotData, -1, 1000, onProgress)
         await this._oplog.join(log)
         await this._updateIndex()
         this.events.emit('replicated', this.address.toString())
